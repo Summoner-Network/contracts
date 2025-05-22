d835162cc6bbfc9a0833bcc89ead8f4a65cff890
definenoob <egd@summoner.to>
2025-05-22 13:28:00 -0400

    ignore

--- types/0_core.ts
+++ types/0_core.ts
@@ -8,24 +8,55 @@ interface Elm {
 type Pointer<T extends Elm> = [typeIndex: number, address: u64];
 
 const enum EdgeType {
-    ElmAccountFactors = 0,
-    ElmFactorSecures = 1
+    ElmAccountFactors = "has_factor",
+    ElmFactorSecures = "is_securing",
+    FactorDelegates = "is_delegating",
+    FactorSubordinates = "is_suborning",
 }
 
 const enum ElmType {
-    Account = 1,
-    AccountFactorNormal = 2,
-    AccountFactorCrypto = 3,
-    
-    FactorDelegates = 4,
-    FactorSubordinates = 5,
+    Unk = 0,
+    Map = 1,
+    Vec = 2,
+    Set = 3,
+    Account = 1000,
+    AccountFactorNormal = 1001,
+    AccountFactorCrypto = 1002,
 }
 
-/** TODO: Create an Elm from these
-  type: number;
-  id: bigint;
-  attributes: { [key: string]: string };
+export class ElmUnk implements Elm {
+    static readonly index: ElmType = ElmType.Unk;
+    readonly index: ElmType = ElmUnk.index;
+    readonly id: u64
+    // other fields
+    data: string
+}
+
+export class ElmMap implements Elm {
+    static readonly index: ElmType = ElmType.Map;
+    readonly index: ElmType = ElmMap.index;
+    readonly id: u64
+    // other fields
+    length: u64
+}
+
+/*
+export class ElmVec implements Elm {
+    static readonly index: ElmType = ElmType.Vec;
+    readonly index: ElmType = ElmVec.index;
+    readonly id: u64
+    // other fields
+    length: u64
+}
 */
+
+export class ElmSet implements Elm {
+    static readonly index: ElmType = ElmType.Set;
+    readonly index: ElmType = ElmSet.index;
+    readonly id: u64
+    // other fields
+    length: u64
+}
 
 function createElm(type: number, id: u64, attributes: { [key: string]: string }): Elm {
     return {
