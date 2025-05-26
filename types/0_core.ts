5b904d189d3fccd8ddc10c90f5d2e0dd8f954d49
definenoob <egd@summoner.to>
2025-05-26 18:36:42 -0400

    changes (not ready yet)

--- types/0_core.ts
+++ types/0_core.ts
@@ -1,8 +1,8 @@
-type u64 = bigint;
+export type u64 = bigint;
 
-interface Elm {
+export interface Elm {
     readonly index: number;
-    readonly id: u64;
+    readonly id: u64 | undefined;
 }
 
 type Pointer<T extends Elm> = [typeIndex: number, address: u64];
@@ -14,62 +14,14 @@ const enum EdgeType {
     FactorSubordinates = "is_suborning",
 }
 
-const enum ElmType {
-    Unk = 0,
-    Map = 1,
-    Vec = 2,
-    Set = 3,
+export const enum ElmType {
     Account = 1000,
     AccountFactorNormal = 1001,
     AccountFactorCrypto = 1002,
-}
+    Balance = 2000,
+    Payment = 2001,
+    Exchange = 2002,
+    Asset = 2003,
 
-export class ElmUnk implements Elm {
-    static readonly index: ElmType = ElmType.Unk;
-    readonly index: ElmType = ElmUnk.index;
-    readonly id: u64
-    // other fields
-    data: string
-}
-
-export class ElmMap implements Elm {
-    static readonly index: ElmType = ElmType.Map;
-    readonly index: ElmType = ElmMap.index;
-    readonly id: u64
-    // other fields
-    length: u64
-}
-
-/*
-export class ElmVec implements Elm {
-    static readonly index: ElmType = ElmType.Vec;
-    readonly index: ElmType = ElmVec.index;
-    readonly id: u64
-    // other fields
-    length: u64
-}
-*/
-
-export class ElmSet implements Elm {
-    static readonly index: ElmType = ElmType.Set;
-    readonly index: ElmType = ElmSet.index;
-    readonly id: u64
-    // other fields
-    length: u64
-}
-
-function createElm(type: number, id: u64, attributes: { [key: string]: string }): Elm {
-    return {
-        index: type,
-        id,
-        ...attributes,
-    };
-}
-
-function loadElm<T extends Elm>(type: number, id: u64, attributes: { [key: string]: string }): T {
-    const elm = createElm(type, id, attributes);
-    if (!('index' in elm) || !('id' in elm)) {
-        throw new Error('Invalid Elm object');
-    }
-    return elm as T;
+    Index = 3000,
 }
